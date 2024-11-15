8df664d0aa5ca27412e533ce407b104aae3437d8
NULGEN <git@stackblitz.com>
2024-11-15 23:05:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ContextApi
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/NULGEN/react-ContextApi)
---
+++ src/components/Product.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { ScProduct } from './scParts';
+import { CartContext } from '../contexts/CartContext';
+
+const Product = ({ product }) => {
+  const { addItem } = useContext(CartContext);
+
+  return (
+    <ScProduct>
+      <img src={product.image} alt={`${product.title} book`} />
+      <div className="details">
+        <h1 className="title">{product.title}</h1>
+        <div className="footer">
+          <p className="price">${product.price}</p>
+          <button onClick={() => addItem(product)}>Add to cart</button>
+        </div>
+      </div>
+    </ScProduct>
+  );
+};
+
+export default Product;
